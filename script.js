41348172c41c4d9dad197b29b63340cfce123249
Ashish Kumar Senapati <53336453+ash0202@users.noreply.github.com>
2021-09-23 22:59:52 +0530

    updated

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-/*const counters=document.querySelectorAll('#value');
-counters.forEach(counter => {
-    counter.innerHTML='10';
-    const updateCounter=()=>{
-        const c=+counter.innerHTML;
-        if(c>=1)
-        {
-            counter.innerHTML=c-1;
-            setTimeout(updateCounter, 1000);
-            console.log(c)
-        }
-        else
-        counter.innerHTML=1;
-    }
-    updateCounter();
-});*/
+
 window.onload=function(){
 let counter=10;
 for(let i=1;i<=10;i++)
